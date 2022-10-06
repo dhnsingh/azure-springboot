fe728b7f7ea3220872f3d8541e4e9c9b25687b2c
dhnsingh <dhnsingh@gmail.com>
2022-10-06 17:34:26 +0530

    Update AzureSpringbootApplication.java
    first  commit

--- src/main/java/com/azuretech/azurespringboot/AzureSpringbootApplication.java
+++ src/main/java/com/azuretech/azurespringboot/AzureSpringbootApplication.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@@RestController
 public class AzureSpringbootApplication {
+	
+	GetMapping("/message")
+	public String message(){
+		return "this is the forst commit....";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureSpringbootApplication.class, args);

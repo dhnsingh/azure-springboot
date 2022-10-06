b95ef968daceb664f99b61e16b1d3b9e555d4fae
dhnsingh <dhnsingh@gmail.com>
2022-10-06 16:14:51 +0530

    first commit

---
+++ src/test/java/com/azuretech/azurespringboot/AzureSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azuretech.azurespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
